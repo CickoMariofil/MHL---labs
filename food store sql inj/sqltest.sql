2dfe09612d7a95184a611312c68dfc4ae96a62d0
CickoMariofil <mariofilcicko@gmail.com>
2026-04-13 23:13:00 +0200

    Add food store sql injection

---
+++ food store sql inj/sqltest.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, password, address, isPro) VALUES 
+('Mario', 
+'TW9qc2lub3ZpYw==', 
+'QnVkYXBlc3QsIEZlaMOpciBIb2xsw7MgdXRjYSAyLCAxMDk3IEh1bmdhcnk=', 
+0)
+
+Mario' , 'dGVzdDEyMzQ=', 'test address', 1) /*
